5ed205bc9ba5f42e1b4e61c06681234b0ab58fda
rolandwong212 <rolandwong212@gmail.com>
2017-05-31 12:34:22 -0700

    initial commit for 004.py

---
+++ 004.py
@@ -0,0 +1,18 @@
+# Largest palindrome product
+# Problem 4
+# A palindromic number reads the same both ways. The largest palindrome made
+# from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+def isPalindrome(string):
+    """Returns boolean value"""
+    # string is zero or one char? -- return yes
+    # is first char same as last? -- strip first, last, call isPalindrome on
+    # stripped string
+    # return no
+    pass
+
+
+
+
