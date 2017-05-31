228b9ccb58baa08385ecf88ee5eef15b8b952e92
rolandwong212 <rolandwong212@gmail.com>
2017-05-31 15:48:06 -0700

    fleshed out isPalindrome on 004.py

--- 004.py
+++ 004.py
@@ -8,10 +8,18 @@
 def isPalindrome(string):
     """Returns boolean value"""
     # string is zero or one char? -- return yes
+    if len(string) is 0:
+        return true
+    elif len(string) is 1:
+        return true
+
     # is first char same as last? -- strip first, last, call isPalindrome on
     # stripped string
-    # return no
-    pass
+    if string[0] is string[-1]:
+        isPalindrome(string[1:len(string)-1])
+        
+    # otherwise return no
+    return false
 
 #isPalindrome(makestring(999*range(999 to 1)))
 # for i in range 999 to 1
