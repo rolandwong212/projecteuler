3d90943666ae5d3f312b53859c9361d6afecb717
rolandwong212 <rolandwong212@gmail.com>
2017-06-27 06:34:55 -0700

    000.py with generic code to time execution

---
+++ 000.py
@@ -0,0 +1,14 @@
+# time for calculation of execution time
+import time
+
+# time at the start of program execution
+start_execution = time.time()
+
+# code goes here
+
+
+# time at the end of execution
+end_execution = time.time()
+
+# printing the total time taken
+print end_execution - start_execution
